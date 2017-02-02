56c4e72fa5319b1a473511a207e7c9e71d642f51
Marcus <best.nose@gmail.com>
2017-02-02 13:26:33 +1300

    new fix for poor timer after cancel performance

--- grinder.ino
+++ grinder.ino
@@ -302,6 +302,8 @@ void loop() {
   #ifdef debug
     Serial.print("State is ");
     Serial.print(state);
+    Serial.print("    Grind start time is: ");
+    Serial.print(grind_start);
     Serial.print("     Mode is: ");
     Serial.println(mode);
    #endif
